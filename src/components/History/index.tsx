1909ac9ea3e8d6d4ba03670487c5c5e13d58028e
Juan Guilherme <asjuanguilherme@gmail.com>
2022-05-09 13:00:16 -0300

    feat: sort history items descending by date

--- src/components/History/index.tsx
+++ src/components/History/index.tsx
@@ -9,16 +9,18 @@ interface Props {
 
 const History = (props: Props) => {
   const renderHistoryList = () =>
-    props.data.map(item => (
-      <HistoryItem
-        key={item.id}
-        id={item.id}
-        date={item.date}
-        title={item.title}
-        value={item.value}
-        category={item.category}
-      />
-    ))
+    props.data
+      .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+      .map(item => (
+        <HistoryItem
+          key={item.id}
+          id={item.id}
+          date={item.date}
+          title={item.title}
+          value={item.value}
+          category={item.category}
+        />
+      ))
 
   return <S.HistoryList>{renderHistoryList()}</S.HistoryList>
 }
